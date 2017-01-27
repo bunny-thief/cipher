b63ace456a872e5ec290dff8eec2fdae3e8d6625
Jacques Navarro <jacques.navarro@gmail.com>
2017-01-27 01:18:42 -0500

    Changes to working branch
    - removed key parameter from cipherN function
    - shift number now entered at end of string
    - RegEx used to assign shift number
    - RegEx used to remove number at end of string
    - RegEx used to read negative numbers
    - Tried to use negative shift numbers (shift not working)

--- cipher.js
+++ cipher.js
@@ -1,8 +1,15 @@
 //caps and lowercase letters
-function cipherN(str, key) { // LBH QVQ VG!
+function cipherN(str) { // LBH QVQ VG!
 
 var arr = [];
 var arr2 = [];
+
+//capture number at end of string, convert to string
+var key = /-?\d+$/g.exec(str);
+key = Number(key[0]);
+
+//remove number at end of string
+str = str.substr(0, (str.search(/-?\d+$/g) - 1));
 
 //split str into an array of chars
   arr = str.split("");
@@ -13,26 +20,25 @@ arr.forEach(function(e) {
   });
   //console.log("charCodeAt: arr2" + arr2);
 
+//positive number
+if(key > 0) {
 arr2.forEach(function(e) {
   //A - M
   if (e > 64 && e < 78) {
     arr.push(e + key);
     arr.shift();
   }
-
   //N - Z
   else if (e > 77 && e < 91) {
     arr.push(64 + (e - 90 + key));
     //arr.push(e - key);
     arr.shift();
   }
-
   //a - m
   else if (e > 96 && e < 110) {
     arr.push(e + key);
     arr.shift();
   }
-
   //n - z
   else if (e > 109 && e < 123) {
     arr.push(96 + (e - 122 + key));
@@ -45,13 +51,42 @@ arr2.forEach(function(e) {
     arr.shift();
   }
  });
-
-  //console.log("
+}
+ //negative number
+ else if (key < 0) {
+   arr2.forEach(function(e) {
+//A-M
+  if (e > 64 && e < 78) {
+    arr.push((90 + e)- (64 + key));
+    arr.shift();
+  }
+  //N-Z
+  else if (e > 77 && e < 91) {
+    arr.push(e - key);
+    arr.shift();
+  }
+//a-m
+  else if (e > 110 && e < 123) {
+    arr.push(e - key);
+    arr.shift();
+  }
+//n-z
+  else if (e > 96 && e < 109) {
+  arr.push((122 + e)- (96 + key));
+    arr.shift();
+  }
+  else {
+    arr.push(e);
+    arr.shift();
+  }
+ });
+ }
+  //convert back to char
   arr.forEach(function(e) {
     arr2.push(String.fromCharCode(e));
     arr2.shift();
   });
-
+//join chars in array back to a string
   str = arr2.join("");
   return str;
 }
